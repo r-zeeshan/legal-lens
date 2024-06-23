6d1edf22301f12ff0492e69a4e7c02e2fe8750b8
Zeeshan <fiverr.zeeshanrajpoot@gmail.com>
2024-06-23 13:39:02 +0000

    small bug fixes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 import gcsfs
 
+# Load environment variables
 load_dotenv()
 api_key = os.getenv('PINECONE_API_KEY')
 gcs_bucket = os.getenv('GCS_BUCKET')
@@ -16,12 +17,12 @@ def load_dataset():
     gcs_file_path = f'gs://{gcs_bucket}/cleaned_data.csv'
     fs = gcsfs.GCSFileSystem()
     df = pd.read_csv(gcs_file_path, storage_options={'gcsfs': fs})
+    df = df[['id', 'majority_opinion']]  
     return df
 
 df_cleaned = load_dataset()
 
-# Creating a lookup dictionary for faster access
-case_lookup = {int(row['id']): row for _, row in df_cleaned.iterrows()}
+case_lookup = {int(row['id']): row['majority_opinion'] for _, row in df_cleaned.iterrows()}
 
 @st.cache_resource
 def load_model():
@@ -37,23 +38,14 @@ def get_case_summaries(input_text, top_k=5):
         try:
             case_id_int = int(float(case_id))
             if case_id_int in case_lookup:
-                case_data = case_lookup[case_id_int]
-                summary = summarize_text(case_data['majority_opinion'])
-                summaries.append({'case_id': case_id_int, 'summary': summary, 'details': case_data})
+                case_text = case_lookup[case_id_int]
+                summary = summarize_text(case_text)
+                summaries.append({'case_id': case_id_int, 'summary': summary})
         except Exception as e:
             st.error(f"Error processing case_id {case_id}: {e}")
     
     return summaries
 
-def display_case_details(case_details):
-    st.markdown(f"**ID**: {case_details['id']}")
-    st.markdown(f"**Name**: {case_details['name']}")
-    st.markdown(f"**Decision Date**: {case_details['decision_date']}")
-    st.markdown(f"**Docket Number**: {case_details['docket_number']}")
-    st.markdown(f"**Court**: {case_details['court']}")
-    st.markdown(f"**Jurisdiction**: {case_details['jurisdiction']}")
-    st.markdown(f"**Majority Opinion**: {case_details['majority_opinion']}")
-    st.markdown(f"**Opinion Length**: {case_details['opinion_length']}")
 
 ### APP LAYOUT
 st.title('LegalLens - Precedence Lookup')
@@ -74,10 +66,8 @@ if st.button('Find Similar Cases'):
         for i, case in enumerate(case_summaries):
             with st.expander(f'Case {i+1} (ID: {case["case_id"]})'):
                 st.write(case['summary'])
-                if st.button(f"View full details of Case {i+1}", key=f"button_{case['case_id']}"):
-                    display_case_details(case['details'])
 
-# Footer
+
 st.markdown(
     """
     <style>
